248a0007fa991480a00d7c6becc1247b7027b815
PabTro <pablord@gmail.com>
2020-05-03 17:13:26 -0400

    Updates

---
+++ precache-manifest.7c4a8823174b926d511947305010bee4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "385d4db04da715c2c14ceeaff4a11ce4",
+    "url": "/lista/index.html"
+  },
+  {
+    "revision": "d2313ec1b08c55cdaf81",
+    "url": "/lista/static/css/main.6feebb32.chunk.css"
+  },
+  {
+    "revision": "190f7bb580a6fd890b00",
+    "url": "/lista/static/js/2.8d74c928.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/lista/static/js/2.8d74c928.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2313ec1b08c55cdaf81",
+    "url": "/lista/static/js/main.b26d8376.chunk.js"
+  },
+  {
+    "revision": "50843c7086e8addba117",
+    "url": "/lista/static/js/runtime-main.392f0a13.js"
+  }
+]);
