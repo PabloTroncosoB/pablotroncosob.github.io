4de770afa8d568e9663e0b53e0d338a1adf4ade2
pablotroncosob <pablotroncosob@gmail.com>
2020-05-11 17:14:57 -0400

    cv

---
+++ precache-manifest.17e41f474b3caa8c61dd364d46eb58dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a3ac67b1df3e3f3c1d223e7fdc79235",
+    "url": "/index.html"
+  },
+  {
+    "revision": "766f9ec7b3f2451512f9",
+    "url": "/static/css/main.eab24d3c.chunk.css"
+  },
+  {
+    "revision": "24421903787e3326d74a",
+    "url": "/static/js/2.4da0241c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4da0241c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "766f9ec7b3f2451512f9",
+    "url": "/static/js/main.f52b6f1e.chunk.js"
+  },
+  {
+    "revision": "c48d943e6f5983c1bd3d",
+    "url": "/static/js/runtime-main.4e108d9b.js"
+  }
+]);
