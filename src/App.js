6081a0cb6305a4c0f976ba82688db58620bb0120
pablotroncosob <pablotroncosob@gmail.com>
2020-05-11 21:56:33 -0400

    first

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Head from './comp/head';
+
+function App() {
+  return (
+    <div className="App">
+        <Head />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/comp/head.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Head() {
+    return (
+        <div className="Head">
+         Cabeza
+        </div>
+    );
+}
+
+export default Head;
