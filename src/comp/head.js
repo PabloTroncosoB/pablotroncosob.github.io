beddf444cba86c0d59facd7a0bac61adf25327aa
PabTro <pablotroncosob@gmail.com>
2021-03-06 01:30:04 -0300

    a

--- src/comp/head.js
+++ src/comp/head.js
@@ -18,7 +18,7 @@ function Head() {
             
 			<h2>Ingeniero de software full stack</h2>
             Profesional con 9 años de experiencia en desarrollo de software, apasionado por las nuevas tecnologías y lenguajes de programación.
-            <br/>En busca de un empleo desafiante donde pueda ampliar mi perfil profesional, aprender, capacitarme y ganar experiencia creando software robusto y escalable que marque la diferencia.
+            <br/>Mis motivaciones son: ampliar mi perfil profesional, aprender, capacitarme, ganar experiencia, crear software robusto y escalable que marque la diferencia.
         </div>
     );
 }
