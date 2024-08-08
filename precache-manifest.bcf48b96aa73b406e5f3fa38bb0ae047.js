7bfd8333ac97e4501d14c0e7b56cca0dab784438
Pablo <pablord@gmail.com>
2024-08-08 00:45:49 -0400

    super update con idioma

---
+++ precache-manifest.bcf48b96aa73b406e5f3fa38bb0ae047.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "771474e2d5763b76afab1ab6f08ff306",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7a6f14a1af4509b9b8f9",
+    "url": "/static/css/main.ba990213.chunk.css"
+  },
+  {
+    "revision": "1b6bb366d315c9fc3632",
+    "url": "/static/js/2.03473a71.chunk.js"
+  },
+  {
+    "revision": "7a6f14a1af4509b9b8f9",
+    "url": "/static/js/main.9938d07c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
